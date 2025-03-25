6ab29f97bbd0fcfea1269ebd1b442a1442a9a9db
Roland Schmitz <roland@2rs.de>
2025-03-25 08:36:46 +0100

    Initial Commit

---
+++ SwiftUI-Minigolf/SwiftUI_MinigolfApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_MinigolfApp.swift
+//  SwiftUI-Minigolf
+//
+//  Created by Roland Schmitz on 25.03.25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_MinigolfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
